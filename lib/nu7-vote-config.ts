b12b900ff4e92411bd6def3bcfcede7e800c650e
Antoine Serval <k6nb4k@gmail.com>
2026-08-25 06:11:44 +1000

    fix(governance): correct vote start time to Aug 25 15:00 UTC

--- lib/nu7-vote-config.ts
+++ lib/nu7-vote-config.ts
@@ -2,7 +2,7 @@ export const NU7_VOTE = {
   title: 'NU7 Coinholder Vote',
   snapshotHeight: 3459350,
   snapshotTime: '2026-08-24T19:00:00Z',
-  voteStartTime: '2026-08-25T00:00:00Z',
+  voteStartTime: '2026-08-25T15:00:00Z',
   voteEndTime: '2026-09-14T19:00:00Z',
   legitimacyThreshold: 1_000_000,
   forumUrl: 'https://forum.zcashcommunity.com/t/nu7-coinholder-vote/56912',
